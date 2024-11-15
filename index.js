4952ce30f42d4cbb013f946166fa7324faf37a8e
Brayden Watson <bwat1@byu.edu>
2024-11-14 17:45:25 -0700

    tried to link database with the login data. hopefully it works

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,6 +9,23 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+
+// MongoDB connection
+const mongoURI = 'mongodb+srv://bwatsonbyu:byubrayden100@cluster0.fg9hf.mongodb.net/pushupTracker?retryWrites=true&w=majority';
+
+const client = new MongoClient(mongoURI);
+
+async function connectToDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectToDB();;
 
 const users = [
   { email: 'user@example.com', password: 'password123' }
